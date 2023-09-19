a0097244724879b5ccc59ed70fe2f33713199f7b
Simon <simon.metzler@student.htldornbirn.at>
2023-09-19 08:56:22 +0200

    finished Java_IDE_Intro

---
+++ src/at/sim/units/Java_IDE_Intro.java
@@ -0,0 +1,15 @@
+package at.sim.units;
+
+public class Java_IDE_Intro {
+    public static void main(String[] args) {
+
+        int x = 7; //ganze zahl
+        float f = 4.4f; //komma zahl
+        boolean isValid = true;
+        String text = "hallo";
+
+        System.out.print("X: " + x + text);
+        System.out.println("X: " + x + text);
+        System.out.println("X: " + x + text);
+    }
+}
