3c19cd970395dbd864afc6fd3e90615855f479a0
Simon <simon.metzler@student.htldornbirn.at>
2023-09-22 08:58:18 +0200

    finished Switch_Aufgabe_1

---
+++ src/at/sim/units/Switch_Aufgabe_1.java
@@ -0,0 +1,37 @@
+package at.sim.units;
+
+import java.util.Random;
+public class Switch_Aufgabe_1 {
+    public static void main(String[] args) {
+        // Erstelle eine Zufallszahl zwischen 5 und 10
+
+        // Wenn der Wert 10 ist gib aus Ten
+        // Wenn der Wert 9 ist gib aus Nine
+        // Wenn der Wert 8 ist gib aus Eight
+        // etc.
+
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(5, 11);
+
+        switch (randomNumber1){
+            case 10:
+                System.out.println("Ten");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+        }
+    }
+}
