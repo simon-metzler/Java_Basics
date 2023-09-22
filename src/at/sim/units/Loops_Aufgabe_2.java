709d1381739e8dd5ac0ce6a6c648acbc62f09aa8
Simon <simon.metzler@student.htldornbirn.at>
2023-09-22 09:09:45 +0200

    finished Loops_Aufgabe_2

---
+++ src/at/sim/units/Loops_Aufgabe_2.java
@@ -0,0 +1,12 @@
+package at.sim.units;
+
+public class Loops_Aufgabe_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i <=1000; i+=2){
+            sum += i;
+
+        }
+        System.out.println(sum);
+    }
+}
