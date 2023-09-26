fc95128cf08cad80a289f57f3081a0efa49ce253
Simon <simon.metzler@student.htldornbirn.at>
2023-09-26 09:35:27 +0200

    started Bankomat

---
+++ src/at/sim/exercises/Bankomat.java
@@ -0,0 +1,16 @@
+package at.sim.exercises;
+
+import java.util.Scanner;
+public class Bankomat {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            scanner.nextInt();
+
+        }
+
+    }
+}
