d697d244577edbeec0a47570bb444da13e759706
Simon <simon.metzler@student.htldornbirn.at>
2023-09-29 08:18:56 +0200

    finished Bankomat

--- src/at/sim/exercises/Bankomat.java
+++ src/at/sim/exercises/Bankomat.java
@@ -1,14 +1,44 @@
 package at.sim.exercises;
 
 import java.util.Scanner;
+
 public class Bankomat {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int bal = 0;
 
+
+        System.out.println("Bankomat" +
+                "\n 1. Einzahlen" +
+                "\n 2. Abheben" +
+                "\n 3. Kontostand" +
+                "\n 4. Beenden");
 
         while (true) {
-            scanner.nextInt();
+
+            System.out.println("Eingabe: ");
+            int prompt = scanner.nextInt();
+
+            if (prompt == 1) {
+                System.out.println("Geben Sie den Betrag ein den Sie einzahlen möchten: ");
+                int amount = scanner.nextInt();
+                bal += amount;
+                System.out.println("Es wurden " + amount + " eingezahlt.");
+            }
+            if (prompt == 2) {
+                System.out.println("Wie viel möchten Sie gerne abheben: ");
+                int amount = scanner.nextInt();
+                bal -= amount;
+                System.out.println("Es wurden " + amount + " abgehoben.");
+            }
+            if (prompt == 3) {
+                System.out.println("Kontostand: " + bal + "€");
+            }
+            if (prompt == 4) {
+                break;
+            }
+
 
         }
 
