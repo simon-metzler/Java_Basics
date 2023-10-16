5ad86ac3bd94e1ddcb1f43210f53f6a952464e8e
Simon <simon.metzler@student.htldornbirn.at>
2023-10-16 14:39:14 +0200

    worked on TicTacToe

--- src/at/sim/games/TicTacToe.java
+++ src/at/sim/games/TicTacToe.java
@@ -10,9 +10,25 @@ public class TicTacToe {
 
         while (game_is_running) {
             System.out.println("Player 1: ");
-            int[] position = getPositionFromInput(scanner.next());
+            int[] position = getPosition(scanner);
+            while (!checkPositionIsFree(board, position)) {
+                System.out.println("Position is not free");
+                System.out.println("New position: ");
+                position = getPosition(scanner);
+            }
             updateBoard(board, position, 1);
             printBoard(board);
+
+            System.out.println("Player 2: ");
+            int[] position2 = getPosition(scanner);
+            while (!checkPositionIsFree(board, position2)) {
+                System.out.println("Position is not free");
+                System.out.println("New position: ");
+                position2 = getPosition(scanner);
+            }
+            updateBoard(board, position2, 2);
+            printBoard(board);
+
 
 
 
@@ -23,15 +39,16 @@ public class TicTacToe {
         for (int[] row : board) {
             for (int state : row) {
                 if(state == 1){
-                    System.out.print("o|");
+                    System.out.print("|o");
                 }
-                if(state == 2){
-                    System.out.print("x|");
+                else if(state == 2){
+                    System.out.print("|x");
                 }
                 else {
-                    System.out.print(" |");
+                    System.out.print("| ");
                 }
             }
+            System.out.print("|");
             System.out.println();
         }
     }
@@ -43,9 +60,23 @@ public class TicTacToe {
         return output;
     }
 
+    public static int[] getPosition(Scanner scanner){
+        int[] position = getPositionFromInput(scanner.next());
+        return position;
+    }
+
     public static int[][] updateBoard(int[][] board, int[] position, int state){
         board[position[0]][position[1]] = state;
         return board;
+    }
+
+    public static boolean checkPositionIsFree (int [][] board, int[] position) {
+        if (board[position[0]][position[1]] == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
