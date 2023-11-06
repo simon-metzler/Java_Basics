4c593fc96b5ab81b3ed3b67facab21b0dec3bd2f
Simon <simon.metzler@student.htldornbirn.at>
2023-11-06 14:46:57 +0100

    worked on OO_Car

--- src/at/sim/units/OO_Car/Car.java
+++ src/at/sim/units/OO_Car/Car.java
@@ -4,11 +4,37 @@ public class Car {
     private String color;
     public int fuelAmount;
     public int fuelConsumption;
+
+    public int tankVolume;
     public String serialNumber;
     public String brand;
 
     public void drive(){
         this.fuelAmount = this.fuelAmount - fuelConsumption;
         System.out.println("I am driving");
+    }
+
+    public void brake() {
+        System.out.println("Ich bremse!");
+    }
+
+    public void turboBoost() {
+        if ((fuelAmount/tankVolume)>0.1) {
+            System.out.println("Super boost");
+        } else {
+            System.out.println("Not enough fuel");
+        }
+
+    }
+
+    public void honk(int amountOfRepetitions) {
+        for (int i = 0; i < amountOfRepetitions; i++){
+            System.out.println("Tuuuut");
+        }
+    }
+
+    public void getRemainingRange() {
+        float remainingRange = (float) fuelAmount / fuelConsumption;
+        System.out.println("Remaining range: " + remainingRange);
     }
 }
--- src/at/sim/units/OO_Car/Main.java
+++ src/at/sim/units/OO_Car/Main.java
@@ -6,18 +6,26 @@ public class Main {
         c1.brand = "Audi";
         c1.fuelConsumption = 7;
         c1.serialNumber = "A1234";
-        c1.fuelAmount = 70;
+        c1.tankVolume = 100;
+        c1.fuelAmount = 20;
 
         Car c2 = new Car();
         c2.brand = "BMW";
         c2.fuelConsumption = 6;
         c2.serialNumber = "B1234";
+        c1.tankVolume = 50;
         c2.fuelAmount = 20;
 
 
         System.out.println(c2.fuelAmount);
         c2.drive();
         System.out.println(c2.fuelAmount);
+
+        c1.turboBoost();
+
+        c1.honk(10);
+
+        c1.getRemainingRange();
 
 
     }
