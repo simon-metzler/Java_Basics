4e9fa81232e5e5e5952c25b0d399d827add788d7
Simon <simon.metzler@student.htldornbirn.at>
2023-12-13 09:26:32 +0100

    finished lamp

--- src/at/sim/exercises/lamp/LightElement.java
+++ src/at/sim/exercises/lamp/LightElement.java
@@ -2,10 +2,38 @@ package at.sim.exercises.lamp;
 
 public class LightElement {
 
+    private String name;
+    private String color;
+    private int powerUsage;
+    private boolean state;
 
+    public LightElement(String name, String color) {
+        this.name = name;
+        this.color = color;
+        this.powerUsage = 0;
+        this.state = false;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getPowerUsage() {
+        return powerUsage;
+    }
 
     public void turnOn() {
+        if (this.state == true) {
+            System.out.println("Name: " + this.name + ". Already turned on.");
+        }
+        else {
+            this.state = true;
+            this.powerUsage += 5;
+        }
+
+
+
+
 
     }
 
--- src/at/sim/exercises/lamp/Main.java
+++ src/at/sim/exercises/lamp/Main.java
@@ -3,5 +3,21 @@ package at.sim.exercises.lamp;
 public class Main {
     public static void main(String[] args) {
 
+        Lamp lamp = new Lamp();
+
+        LightElement l1 = new LightElement("l1", "green");
+        LightElement l2 = new LightElement("l2", "blue");
+        LightElement l3 = new LightElement("l3", "red");
+
+        lamp.addLightElement(l1);
+        lamp.addLightElement(l2);
+        lamp.addLightElement(l3);
+
+        System.out.println(lamp.getOverallPowerUsage());
+        lamp.turnAllOn();
+        System.out.println(lamp.getOverallPowerUsage());
+        lamp.turnAllOn();
+        lamp.printNamesOfLightElements();
+
     }
 }
